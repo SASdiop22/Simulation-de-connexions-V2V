0ff4a5d5c6f41363a2379e9c9d38d8cca6f571f5
NFSTOURE <151944692+NFSTOURE@users.noreply.github.com>
2024-11-11 10:17:56 +0100

    Update noeud.cpp

--- noeud.cpp
+++ noeud.cpp
@@ -1,16 +1,16 @@
-#include "Noeud.h"
+#include "noeud.h"
 #include <cmath>
 #include "point.h"
 
 const double PI = M_PI;
 
 // Constantes pour la projection Lambert 93
-const double X_OFFSET = 700000; // Déplacement en X
-const double Y_OFFSET = 6600000; // Déplacement en Y
+const double X_OFFSET = 700000; // DÃ©placement en X
+const double Y_OFFSET = 6600000; // DÃ©placement en Y
 const double LAMBERT_A = 6378137; // Rayon de la Terre
 const double LAMBERT_F = 1 / 298.257223563; // Aplatissement
-const double LAMBERT_N = 0.730883; // Paramètre de projection
-const double LAMBERT_E = 5.1227; // Paramètre d'excentricité
+const double LAMBERT_N = 0.730883; // ParamÃ¨tre de projection
+const double LAMBERT_E = 5.1227; // ParamÃ¨tre d'excentricitÃ©
 
 Noeud::Noeud(osmium::object_id_type id, double lat, double lon)
     : id(id), latitude(lat), longitude(lon) {
@@ -34,7 +34,7 @@ point Noeud::getCoordonnees() const{
     return point(X_lambert93(),Y_lambert93());
  }
 
-// Méthodes de conversion Lambert 93
+// MÃ©thodes de conversion Lambert 93
 double Noeud::X_lambert93() const {
     return X_OFFSET + (LAMBERT_A * std::cos(latitude * PI / 180) * std::sin(longitude * PI / 180) / (1 - LAMBERT_E * std::sin(latitude * PI / 180)));
 }
